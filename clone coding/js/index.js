5e7c42eccffa5d9569f3d34c6793fb7e47ab391b
kimsehee <rlatpgml8135@naver.com>
2021-04-14 23:18:10 +0900

    clone coding

--- clone coding/js/index.js
+++ clone coding/js/index.js
@@ -1,10 +1,11 @@
 const menuOpenEl = document.getElementById('btn--open')
-const menuCloseEl = document.getElementById('btn--close')
 const sideMenuEl = document.getElementById('side__menu')
+const menuCloseEl = document.getElementById('btn--close')
 
 menuOpenEl.addEventListener("click", function(){
     sideMenuEl.style.transform = 'translateX(0%)'
 })
+
 
 menuCloseEl.addEventListener("click", function(){
     sideMenuEl.style.transform = 'translateX(-100%)'
